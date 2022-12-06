fc3820f42fa94a31fac474af89b6f0cf18d5f2cd
Sey <hagiosofori@gmail.com>
2022-12-06 17:28:33 +0000

    remove save button
    pass errors and errors setter to pages

--- src/App.jsx
+++ src/App.jsx
@@ -52,6 +52,7 @@ function App() {
 	const [page, setPage] = useState(1);
 
 	const [data, setData] = useState(initialState);
+	const [errors, setErrors] = useState(initialErrorState);
 
 	async function fetchcategories() {
 		const result = await categoryfxn();
@@ -75,7 +76,12 @@ function App() {
 				<Image src="/logo.png" />
 			</div>
 
-			<CurrentPage data={data} setData={setData} />
+			<CurrentPage
+				data={data}
+				setData={setData}
+				errors={errors}
+				setErrors={setErrors}
+			/>
 
 			<br />
 			<br />
@@ -91,8 +97,7 @@ function App() {
 				<ButtonGroup>
 					{page === pages.length ? (
 						<Button variant="secondary">Preview</Button>
-					) : null //<Button>Save</Button>
-					}
+					) : null}
 
 					{page < pages.length ? (
 						<Button
