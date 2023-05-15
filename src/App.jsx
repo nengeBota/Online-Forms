3b6ce9e9a8d5ecccfad25d660266180caee4d791
Sey <hagiosofori@gmail.com>
2023-05-15 13:05:36 +0000

    remove border

--- src/App.jsx
+++ src/App.jsx
@@ -266,7 +266,7 @@ const PageWrapper = styled.div`
 	display: flex;
 	flex-direction: column;
 	margin: 0 auto;
-	border: 1px solid black;
+	/* border: 1px solid black; */
 	min-height: 100vh;
 	padding: 30px;
 
